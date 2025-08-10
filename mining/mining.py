b690c2bbc89d3e8ebb75a638816e6fcd1e66cd34
ISTEK <tamdacoder@gmail.com>
2025-08-09 21:37:17 -0500

    Update mining.py

--- mining/mining.py
+++ mining/mining.py
@@ -12,21 +12,23 @@ def diff():
   ranges = random.randint(1, 32)
   drn = os.urandom(ranges)
   diff256 = hashlib.sha256(drn).hexdigest()
-  return diff256
+  return int(diff256, 16)
 
 t = (2**256 - 1) / diff()
-target = t.to_bytes(32, bytesorder="big")
+target = t.to_bytes(32, byteorder="big")
 prevHash = rng()
 merkle = rng()
 ti = time.time()
-timestamp = ti.to_bytes(32, bytesorder="big")
-version = 1
+timestamp = int(ti).to_bytes(8, byteorder="big")
+v = 1
+version = v.to_bytes(4, byteorder="big")
 difficulty = diff()
 targ = target
 
 
 def pseudoblock():
-  nonce = random.randint(1, 2**32)
+  nun = random.randint(1, 2**32)
+  nonce = nun.to_bytes(4, byteorder="big")
   blockHash = hashlib.sha256(prevHash + merkle + timestamp + version + difficulty + target + nonce).hexdigest()
   return blockHash
 
